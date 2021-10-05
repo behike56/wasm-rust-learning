f57759cc37bdbaa4f51f45ff0a5f94664f3f98c7
behike56 <living.tech.dancing.code@gmail.com>
2021-10-05 23:25:34 +0900

    init

---
+++ hello-wasm/target/rls/debug/deps/hello_wasm-2fd4ebe31a5b6a64.d
@@ -0,0 +1,5 @@
+/home/behike56/00-PlayGround/wasm-rust-learning/hello-wasm/target/rls/debug/deps/hello_wasm-2fd4ebe31a5b6a64.rmeta: src/lib.rs
+
+/home/behike56/00-PlayGround/wasm-rust-learning/hello-wasm/target/rls/debug/deps/hello_wasm-2fd4ebe31a5b6a64.d: src/lib.rs
+
+src/lib.rs:
---
+++ hello-wasm/target/rls/debug/deps/hello_wasm-b755ff7fd082d404.d
@@ -0,0 +1,5 @@
+/home/behike56/00-PlayGround/wasm-rust-learning/hello-wasm/target/rls/debug/deps/hello_wasm-b755ff7fd082d404.rmeta: src/lib.rs
+
+/home/behike56/00-PlayGround/wasm-rust-learning/hello-wasm/target/rls/debug/deps/hello_wasm-b755ff7fd082d404.d: src/lib.rs
+
+src/lib.rs:
